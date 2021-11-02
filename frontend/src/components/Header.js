3e18c30fe59bd8bf9f6f594ab71339d1951c1fdd
Polar <davidchen233@gmail.com>
2021-11-02 10:47:47 +0800

    add header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,53 @@
-import React from 'react';
-
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
+import './header.css';
+import { BsPerson, BsCart3 } from 'react-icons/bs';
 const Header = () => {
-  return <header></header>;
+  const [sticky, setSticky] = useState(false);
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+      setSticky(true);
+    } else {
+      setSticky(false);
+    }
+  });
+  return (
+    <header className={sticky ? 'sticky' : ''}>
+      <NavLink to="/" className="logo">
+        <Logo />
+      </NavLink>
+      <div className="toggle"></div>
+      <nav className="menu">
+        <ul>
+          <li>
+            <NavLink to="home">壓力檢測</NavLink>
+          </li>
+          <li>
+            <NavLink to="#about">預約心理師</NavLink>
+          </li>
+          <li>
+            <NavLink to="#services">紓壓小物</NavLink>
+          </li>
+          <li>
+            <NavLink to="#work">心情聊天室</NavLink>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <NavLink to="home">
+              <BsPerson size="28" />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="home">
+              <BsCart3 size="26" />
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
