1fa237c718e5e9353723ec2997ef42aee20cf4b9
Polar <davidchen233@gmail.com>
2021-11-02 12:22:27 +0800

    edit header rwd

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -12,14 +12,15 @@ const Header = () => {
       setSticky(false);
     }
   });
+  const [toggle, setToggle] = useState(true);
   return (
     <header className={sticky ? 'sticky' : ''}>
       <NavLink to="/" className="logo">
         <Logo />
       </NavLink>
-      <div className="toggle"></div>
-      <nav className="menu">
-        <ul>
+
+      <nav>
+        <ul className={`menu ${toggle ? 'active' : ''}`}>
           <li>
             <NavLink to="home">壓力檢測</NavLink>
           </li>
@@ -33,19 +34,29 @@ const Header = () => {
             <NavLink to="#work">心情聊天室</NavLink>
           </li>
         </ul>
-        <ul>
-          <li>
-            <NavLink to="home">
-              <BsPerson size="28" />
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="home">
-              <BsCart3 size="26" />
-            </NavLink>
-          </li>
-        </ul>
       </nav>
+      <ul className="other-nav">
+        <li>
+          <NavLink to="home">
+            <BsPerson size="28" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="home">
+            <BsCart3 size="26" />
+          </NavLink>
+        </li>
+      </ul>
+      <div
+        className={`toggle ${toggle ? 'active' : ''}`}
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        <div className="bar mb-2"></div>
+        <div className="bar"></div>
+        <div className="bar mt-2"></div>
+      </div>
     </header>
   );
 };
