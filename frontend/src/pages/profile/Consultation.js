d51c1ea4794e97c7ed6a9163d9e48572a0e2c7c0
davidchen233 <davidchen233@gmail.com>
2021-11-08 04:20:35 +0800

    add card swipe argument

--- frontend/src/pages/profile/Consultation.js
+++ frontend/src/pages/profile/Consultation.js
@@ -6,10 +6,13 @@ import Avatar from './tempImg/avatar.jpg';
 
 const Consultation = () => {
   const [swiperPosition, setSwiperPosition] = useState(0);
+  const [rightDisabled, setRightDisabled] = useState('');
   const swipeLengthRef = useRef();
   const swiperWrapperRef = useRef();
   const handleSwipe = () => {
-    console.log(swiperPosition);
+    console.log('position', swiperPosition);
+    console.log('卡片盒寬度', swipeLengthRef.current.offsetWidth);
+    console.log('slider寬度', swiperWrapperRef.current.offsetWidth);
   };
   return (
     <>
@@ -22,6 +25,63 @@ const Consultation = () => {
           className="myConsult-wrapper"
           style={{ left: swiperPosition }}
         >
+          <div className="myConsult-card">
+            <h4>預約資訊</h4>
+            <div className="consult-avatarBx">
+              <img src={Avatar} alt="" />
+            </div>
+            <h5>心理師: 鴨子王</h5>
+            <p>預約日期: 2021/8/9</p>
+            <p>預約時段: 上午(10:00 - 12:00)</p>
+            <p>價格: 2000/次</p>
+            <p>狀態: 未開啟</p>
+            <button>
+              <FaVideo size="22" color="#333" />
+            </button>
+          </div>
+          <div className="myConsult-card">
+            <h4>預約資訊</h4>
+            <div className="consult-avatarBx">
+              <img src={Avatar} alt="" />
+            </div>
+            <h5>心理師: 鴨子王</h5>
+            <p>預約日期: 2021/8/9</p>
+            <p>預約時段: 上午(10:00 - 12:00)</p>
+            <p>價格: 2000/次</p>
+            <p>狀態: 未開啟</p>
+            <button>
+              <FaVideo size="22" color="#333" />
+            </button>
+          </div>
+
+          <div className="myConsult-card">
+            <h4>預約資訊</h4>
+            <div className="consult-avatarBx">
+              <img src={Avatar} alt="" />
+            </div>
+            <h5>心理師: 鴨子王</h5>
+            <p>預約日期: 2021/8/9</p>
+            <p>預約時段: 上午(10:00 - 12:00)</p>
+            <p>價格: 2000/次</p>
+            <p>狀態: 未開啟</p>
+            <button>
+              <FaVideo size="22" color="#333" />
+            </button>
+          </div>
+          <div className="myConsult-card">
+            <h4>預約資訊</h4>
+            <div className="consult-avatarBx">
+              <img src={Avatar} alt="" />
+            </div>
+            <h5>心理師: 鴨子王</h5>
+            <p>預約日期: 2021/8/9</p>
+            <p>預約時段: 上午(10:00 - 12:00)</p>
+            <p>價格: 2000/次</p>
+            <p>狀態: 未開啟</p>
+            <button>
+              <FaVideo size="22" color="#333" />
+            </button>
+          </div>
           <div className="myConsult-card">
             <h4>預約資訊</h4>
             <div className="consult-avatarBx">
@@ -96,8 +156,9 @@ const Consultation = () => {
       </div>
       <div className="d-flex justify-content-center align-items-center">
         <button
-          className="swipeBtn"
+          className={`swipeBtn ${swiperPosition === 0 ? 'disabled' : ''}`}
           onClick={() => {
+            setRightDisabled('');
             if (swiperPosition + 400 >= 0) {
               setSwiperPosition(0);
             } else {
@@ -109,21 +170,20 @@ const Consultation = () => {
           <BsArrowLeftCircle size="36" color="#565656" />
         </button>
         <button
-          className="swipeBtn"
+          className={`swipeBtn ${rightDisabled}`}
           onClick={() => {
             if (
-              swiperPosition <=
-              swiperWrapperRef.current.offsetWidth -
-                swipeLengthRef.current.offsetWidth
+              swipeLengthRef.current.offsetWidth + (swiperPosition - 400) <
+              swiperWrapperRef.current.offsetWidth
             ) {
               setSwiperPosition(
-                swiperWrapperRef.current.offsetWidth -
-                  swipeLengthRef.current.offsetWidth
+                -swipeLengthRef.current.offsetWidth +
+                  swiperWrapperRef.current.offsetWidth
               );
+              setRightDisabled('disabled');
             } else {
               setSwiperPosition(swiperPosition - 400);
             }
-
             handleSwipe();
           }}
         >
