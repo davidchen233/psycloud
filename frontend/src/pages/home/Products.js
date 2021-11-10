b2da245683476beca739c67233ee2f0a90147ff3
davidchen233 <davidchen233@gmail.com>
2021-11-10 21:09:36 +0800

    fix footer height problem

--- frontend/src/pages/home/Products.js
+++ frontend/src/pages/home/Products.js
@@ -4,7 +4,7 @@ import Product from './Product';
 
 const Products = () => {
   return (
-    <section className="h-section">
+    <section className="h-section mb-0">
       <h2>-- 減壓小物 --</h2>
       <div className="d-flex justify-content-between">
         <div></div>
