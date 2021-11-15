e764d7591f485278d132ded9d9ec3525329b97c6
davidchen233 <davidchen233@gmail.com>
2021-11-15 13:10:55 +0800

    add routers in server.js

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,29 @@ app.use(cors({
   credential: true,
 }))
 
+// auth 相關的 API
+let authRouter = require('./routers/auth');
+app.use('/api/auth', authRouter);
 
 // users 相關的 API
-let usersRouter = require('./routers/users')
-app.use('/api/users', usersRouter)
+let usersRouter = require('./routers/users');
+app.use('/api/users', usersRouter);
+
+// tests 相關的 API
+let testsRouter = require('./routers/tests');
+app.use('/api/tests', testsRouter);
+
+// doctors 相關的 API
+let doctorsRouter = require('./routers/doctors');
+app.use('/api/doctors', doctorsRouter);
+
+// products 相關的 API
+let productsRouter = require('./routers/products');
+app.use('/api/products', productsRouter);
+
+// orders 相關的 API
+let ordersRouter = require('./routers/orders');
+app.use('/api/orders', ordersRouter);
 
 
 // 404 not found
