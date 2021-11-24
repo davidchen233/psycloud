fa3c88197e15f138dadc32f006fc56859b6a6840
peilin <kazusho25176@gmail.com>
2021-11-24 23:54:06 +0800

    add orderCompleted page

--- backend/routers/orders.js
+++ backend/routers/orders.js
@@ -13,6 +13,7 @@ function getOrderCode(randomLength) {
 
 // 建立訂單
 router.post("/createOrder", async (req, res) => {
+  // 建立主訂單
   let data = await connection.queryAsync(
     "INSERT INTO orders (order_code, user_id, name, phone, address, total, created_at ) VALUES (?)",
     [
@@ -27,8 +28,9 @@ router.post("/createOrder", async (req, res) => {
       ],
     ]
   );
+  // 處理購買商品
   for (let i = 0; i < req.body.orderItem.length; i++) {
-    //   req.body.orderItem[i]
+    // 建立訂單詳情
     let orderdata = await connection.queryAsync(
       "INSERT INTO order_items (order_id, product_id, amount, price ) VALUES (?)",
       [
@@ -40,6 +42,11 @@ router.post("/createOrder", async (req, res) => {
         ],
       ]
     );
+
+    // 更新商品的銷售量
+    // TODO: 取得當個商品
+    // TODO: 取得當中的 sold 讓他加上購買的數量
+    // TODO: UPDATE 回 商品資料表
   }
   res.json({ code: "0", message: "您已購買成功!!" });
 });
--- frontend/src/pages/cart/loading.js
+++ frontend/src/pages/cart/loading.js
@@ -1,3 +1,15 @@
-export const loading = () => {
-  return <div></div>;
+import './loading.css';
+const Loading = () => {
+  return (
+    <div className="loader-wrapper">
+      <div class="loader">
+        <span></span>
+        <span></span>
+        <span></span>
+        <h2>請稍後...</h2>
+      </div>
+    </div>
+  );
 };
+
+export default Loading;
